282e94b483af66948e999db4557c115ad23a8795
Greg <60011793+gregtuc@users.noreply.github.com>
2020-02-11 23:31:13 -0500

    Making changes to express paths
    Changing express paths to allow for basic user login

--- server/index.js
+++ server/index.js
@@ -1,57 +1,80 @@
 //Basic requirements
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 //Mongo Atlas requirements
-const mongoclient = require('mongodb').MongoClient;
-const objectid = require('mongodb').ObjectID;
-const uri = "mongodb+srv://<username>:<password>@cluster0-pf0b2.mongodb.net/test?retryWrites=true&w=majority";
-const userModel = require('./user-schema');
+const mongoclient = require("mongodb").MongoClient;
+const objectid = require("mongodb").ObjectID;
+const uri =
+  "mongodb+srv://greg34910:cookie123@cluster0-pf0b2.mongodb.net/test?retryWrites=true&w=majority";
+const userModel = require("./user-schema");
 
 //Middleware and CORS
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.use(cors())
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 //Start server
 var database, collection;
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-    mongoclient.connect(uri, { useNewUrlParser: true }, (error, client) => {
-        if (error) {
-            throw error;
-        }
-        database = client.db('memestagram');
-        collection = database.collection('users');
-        console.log('Connected to database: memestagram!');
-    })
-    console.log(`Listening on ${port}`);
+  mongoclient.connect(uri, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+      throw error;
+    }
+    database = client.db("memestagram");
+    collection = database.collection("users");
+    console.log("Connected to database: memestagram!");
+  });
+  console.log(`Listening on ${port}`);
 });
 
 //Default path
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-//Create a user path and accept user details (follows user)
-app.post("/api/user", (request, response) => {
-    const User = new userModel(request.body)
-    collection.insertOne(User, (error, result) => {
-        if (error) {
-            return response.status(500).send(error);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+//Create a user for sign up
+app.post("/signup", (request, response) => {
+  const User = new userModel(request.body);
+  collection.insertOne(User, (error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(User);
+    console.log(`New user created with the following id: ${result.insertedId}`);
+  });
+});
+//Check if a user exists for sign in
+app.post("/login", (request, response) => {
+  var username = request.body.username;
+  var password = request.body.password;
+  collection
+    .findOne({ username })
+    .then(user => {
+      if (user) {
+        console.log(`Success! User found: ${user.username}`);
+        if (password == user.password) {
+          console.log(`Passwords match!`);
+          response.json({ success: true });
+        } else {
+          console.log(`Invalid Password entered!`);
+          response.json({ success: false });
         }
-        response.send(User);
-        console.log(`New user created with the following id: ${result.insertedId}`);
-    });
+      } else {
+        console.log("No users found that match.");
+        response.json({ success: false });
+      }
+    })
+    .catch(err => console.error(`Failed to find document: ${err}`));
 });
 //Search for users
 app.get("/api/users", (request, response) => {
-    collection.find({}).toArray((error, result) => {
-        if (error) {
-            return response.status(500).send(error);
-        }
-        response.send(result);
-    });
+  collection.find({}).toArray((error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
+  });
 });
