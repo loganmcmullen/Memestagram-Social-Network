9209b542086ee43f26f3e3bfb961b166c8467067
Greg <60011793+gregtuc@users.noreply.github.com>
2020-03-06 10:14:59 -0500

    commiting to side-branch for demo

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import LoginSignupPage from "./pages/loginsignup";
 import HomePage from "./pages/homepage";
 import FollowingPage from "./pages/managefollowing";
+import ProfilePage from "./components/profile";
 import "./App.css";
 
 function App() {
@@ -18,6 +19,9 @@ function App() {
           </Route>
           <Route exact path="/following">
             <FollowingPage />
+          </Route>
+          <Route exact path="/profile">
+            <ProfilePage />
           </Route>
         </Switch>
       </div>
