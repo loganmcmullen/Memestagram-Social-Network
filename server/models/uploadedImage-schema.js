fcb58fd137ea115c430a1e507f3381a6224476c7
ginywiny <m_lesk@live.concordia.ca>
2020-03-26 14:06:30 -0400

    Fix authentication with GET and POST for images

--- server/models/uploadedImage-schema.js
+++ server/models/uploadedImage-schema.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 //Save data for image with id, name, description and uploadedImage itself
 const uploadedImageSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    //userID: String,
     name: {type: String, require: true},
     image: {type: String, require: true},
-    uploadedImage: {type: String, required: true}
-
+    uploadedImage: {type: [String], required: true}
+    //imgTest: { data: Buffer, contentType: String}           //Added
 });
 
 module.exports = mongoose.model('UploadedImage', uploadedImageSchema);
--- server/routes/uploadedImage.js
+++ server/routes/uploadedImage.js
@@ -5,6 +5,7 @@ const auth = require("../auth");
 
 const mongoose = require('mongoose');
 const multer = require('multer');
+const fs = require('fs');   
 
 //Save to uploads folder and save image info
 const storage = multer.diskStorage({
@@ -37,7 +38,57 @@ const upload = multer({
 const UploadedImage = require('../models/uploadedImage-schema');
 
 //Get all posts made for uploadedImage
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    UploadedImage.find()
+    .select('name image _id uploadedImage')
+    .exec()
+    .then(docs => {
+        const response = {
+            count: docs.length,
+            images: docs.map(doc => {
+                return {
+                    name: doc.name,
+                    image: doc.image,
+                    uploadedImage: doc.uploadedImage,
+                    _id: doc._id,
+                    //imgTest: doc.data,
+                    //imgTest: doc.contentType,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:8000/api/uploadedImage/' + doc._id
+                    }
+                }
+            }) 
+        };
+        res.status(200).json(response);
+    })
+
+    // UploadedImage.findOne({}, 'img createdAt', function(err, imgTest) {
+    //     if (err)
+    //         res.send(err);
+    //     console.log(imgTest);
+    //     res.contentType('json');
+    //     res.send(imgTest);
+    // }).sort({ createdAt: 'desc' })
+
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+//USE THIS ONE FOR GETTING AND AUTHENTICATING WITH USER---------------------------------------
+//Get all posts made for uploadedImage THAT ARE AUTHENTICATED
+//localhost:8000/api/uploadedImage/authenticated
+router.get('/authenticated', auth, async (req, res, next) => {
+    //Authenticate with user
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      res.status(400).json({ message: "User with matching ID not found" });
+    }
+
     UploadedImage.find()
     .select('name image _id uploadedImage')
     .exec()
@@ -68,13 +119,60 @@ router.get('/', (req, res, next) => {
 });
 
 //Post an image to the DB
-router.post('/', upload.single('uploadedImage'), (req, res, next) => {
+//localhost:8000/api/uploadedImage
+router.post('/', upload.single('uploadedImage'), async (req, res, next) => {    //AFter removed auth
     console.log(req.file);
     const uploadedImage = new UploadedImage({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         image: req.body.image,
-        uploadedImage: req.file.path
+        uploadedImage: req.file.path,
+    })
+    uploadedImage.save()
+    //---------------------------Bad code------------------------------------------
+    // var uploadedImage = new UploadedImage;
+    // uploadedImage.imgTest.data = fs.readFileSync(req.uploadedImage.path)
+    // uploadedImage.imgTest.contentType = 'image/jpeg';
+    // uploadedImage.save()
+    //----------------------------------------------------------------------------
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Created product successfully',
+            createdUploadedImage: {
+                name: result.name,
+                image: result.image,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:8000/api/uploadedImage/' + result._id,
+                }
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    });
+});
+
+//USE THIS ONE FOR UPLOADING AND AUTHENTICATING WITH USER---------------------------------------
+//localhost:8000/api/uploadedImage/authenticated
+//Posts THAT ARE AUTHENTICATED
+router.post('/authenticated', auth, upload.single('uploadedImage'), async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      res.status(400).json({ message: "User with matching ID not found" });
+    }
+
+    console.log(req.file);
+    const uploadedImage = new UploadedImage({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        image: req.body.image,
+        uploadedImage: req.file.path,
     })
     uploadedImage.save()
     .then(result => {
@@ -100,8 +198,9 @@ router.post('/', upload.single('uploadedImage'), (req, res, next) => {
     });
 });
 
+
 //Get exact image from input ID
-router.get('/:uploadedImageId', (req, res, next) => {
+router.get('/:uploadedImageId', async (req, res, next) => {
     const id = req.params.uploadedImageId;
     UploadedImage.findById(id)
     .select('name image _id uploadedImage')
@@ -127,34 +226,34 @@ router.get('/:uploadedImageId', (req, res, next) => {
 });
 
 //Update image details
-router.patch('/:uploadedImageId', (req, res, next) => {
+router.patch('/:uploadedImageId', async (req, res, next) => {
     const id = req.params.uploadedImageId;
     const updateOps = {};
     for (const ops of req.body) {
         updateOps[ops.propName] = ops.value;
     }
     UploadedImage.update({_id: id}, {$set: updateOps})
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.status(200).json({
-            message: 'Product updated',
-            request: {
-                type: 'GET',
-                url: 'http://localhost:8000/api/uploadedImage/' + id
-            }
-        }); 
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            erro: err
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:8000/api/uploadedImage/' + id
+                }
+            }); 
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                erro: err
+            });
         });
-    });
 });
 
 //Delete image using image ID
-router.delete('/:uploadedImageId', (req, res, next) => {
+router.delete('/:uploadedImageId', async (req, res, next) => {
     const id = req.params.uploadedImageId;
     UploadedImage.remove({_id: id})
     .exec()
