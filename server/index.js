a683548fb154aa2fa2cf1346377771faea670cae
loganmcmullen <logan.mcmullen@live.com>
2020-03-29 10:24:22 -0400

    Display 1 uploaded picture to the profile page  #61
    A picture that has been uploaded is now able to be displayed to the profile page.
    
    MUCH more features to come, just comitting a baseline for others to get started on their work.
    
    This was done using JSX. basically implementing javascript functions into HTML tags so that once the html tags are called, it calls the function.
    
    earlier commit included using GridFS and multer to upload the picture
    
    lots of help from @Greg

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
-
-//Initializing express, middleware, and CORS
+//Initializing express, middleware, CORS
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -14,6 +13,9 @@ const config = require("./database/default");
 const uri = config.ConnectionUrl;
 
 const app = express();
+var router = express.Router();
+
+const user = require("./models/user-schema");
 
 //Loading middleware and CORS
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,19 +23,20 @@ app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 app.use(cors());
 
+
 //Initialize database connection
 const connectDatabase = require("./database/connection");
 connectDatabase();
 
+//Middleware and processing for uploading and requesting user photos
+//from the database.
 const conn = mongoose.createConnection(uri);
-
 let gfs;
 conn.once('open', () => {
   // init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
 });
-
 const storage = new GridFsStorage({
   url: uri,
   file: (req, file) => {
@@ -52,32 +55,163 @@ const storage = new GridFsStorage({
     });
   }
 });
-
-const upload = multer({storage});
-
-//Loading router modules
-var login = require("./routes/loginUser");
-app.use("/api/login", login);
-var register = require("./routes/registerUser");
-app.use("/api/register", register);
-var search = require("./routes/searchUser");
-app.use("/api/search", search);
-var currentuser = require("./routes/currentUser");
-app.use("/api/currentuser", currentuser);
+const upload = multer({ storage });
 
 //Listen on Port 8000
 const port = process.env.PORT || 8000;
 
+//Exportable server which is used for integration testing.
 var server = app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
 
+app.use('/uploads', express.static('uploads'));
+
+
 //path POST /upload
+app.post("/uploads", upload.single("myImage"), (req, res) => {
+  console.log(`File: ${req}`);
+  res.json({ file: req.file });
+});
 
 app.post('/uploads', upload.single('myImage' ), (req, res) => {
   console.log(`File: ${req}`)
+  res.json({file: req.file})
   //res.json({file: req.file})
   res.redirect('/');
+
 });
+
+//------------------MICHAEL MISO'S STUFF----------------------------------
+
+
+//Schemas
+const Schema = mongoose.Schema;
+
+//Blog post schema
+const BlogPostSchema = new Schema({
+  title: String,
+  body: String,
+  date: {
+    type: String,
+    default: Date.now()
+  }
+});
+
+//Models
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
+// Route Test
+app.get('/api', (req, res) => {
+  BlogPost.find({  })
+      .then((data) => {
+          console.log('Data: ', data);
+          res.json(data);
+      })
+      .catch((error) => {
+          console.log('error: ', daerrorta);
+      });
+});
+
+//------------------MICHAEL MISO'S STUFF^^^^^----------------------------------
+
+//@route GET /files/:filename
+// @desc Display all files in JSON
+app.get('/files', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if(!files || files.length===0) {
+      return res.status(404).json({
+        err: 'No files exist'
+      });
+    }
+
+    //Files exist
+    return res.json(files);
+  })
+});
+
+
+app.get('/files/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if file
+    if (!file || file.length ===0){
+      return res.status(404).json({
+        err: 'No file exist'
+      });
+    }
+    // File exists
+    return res.json(file);
+  });
+});
+
+//@route GET /image/:filename
+//@desc Display Image
+
+app.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if file
+    if (!file || file.length ===0){
+      return res.status(404).json({
+        err: 'No file exist'
+      });
+    }
+    //Check if image
+
+    if(file.contentType === 'image/jpeg' || 
+    file.contentType === 'image/png' || 
+    file.contentType === 'image/img' || 
+    file.contentType === 'image/jpg' || 
+    file.contentType === 'image/gif' || 
+    file.contentType === 'image/tif') {
+
+      //Read output to browser
+
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    //Check if files exist
+    if(!files || files.length ===0){
+      res.render('profile', {files: false});
+    } else {
+      files.map(file => {
+        if(
+          file.contentType === 'image/jpeg' || 
+          file.contentType === 'image/png' || 
+          file.contentType === 'image/img' || 
+          file.contentType === 'image/jpg' || 
+          file.contentType === 'image/gif' || 
+          file.contentType === 'image/tif') 
+        {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+    }
+    res.sendFile({files:files});
+    res.render('profile', {files: files} );
+    });
+  });
+
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({_id: req.params.id, root: 'upload'}, (err, gridStore) => {
+    if(err){
+      return res.status(404).json({ err: err});
+    }
+    res.redirect('/');
+  });
+});
+
 
 module.exports = server;
