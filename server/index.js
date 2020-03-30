52f67ef3cb920e0e6da851a16d5c61558315dde8
Greg <60011793+gregtuc@users.noreply.github.com>
2020-03-30 14:55:41 -0400

    Issue #64 updating user document after posting photo
    Submitting a photo to the server now updates the submitting user's document in MongoDB with the unique photoid and the description that was posted with the photo.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 //Initializing express, middleware, and CORS
 
 const express = require("express");
@@ -7,17 +6,16 @@ const cors = require("cors");
 const path = require("path");
 const crypto = require("crypto");
 const mongoose = require("mongoose");
-const multer = require("multer")
-const GridFsStorage = require("multer-gridfs-storage")
-const Grid = require("gridfs-stream")
-const methodOverride = require("method-override")
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 const config = require("./database/default");
 const uri = config.ConnectionUrl;
-
+const auth = require("./auth");
 const app = express();
+const User = require("./models/user-schema");
 var router = express.Router();
-
-const user = require("./models/user-schema");
 
 //Loading middleware and CORS
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,7 +33,6 @@ app.use((req,res,next) => {
   }
 })*/
 
-
 //Initialize database connection
 const connectDatabase = require("./database/connection");
 connectDatabase();
@@ -44,10 +41,10 @@ connectDatabase();
 //from the database.
 const conn = mongoose.createConnection(uri);
 let gfs;
-conn.once('open', () => {
+conn.once("open", () => {
   // init stream
   gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+  gfs.collection("uploads");
 });
 const storage = new GridFsStorage({
   url: uri,
@@ -68,7 +65,7 @@ const storage = new GridFsStorage({
   }
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 //Loading router modules
 var login = require("./routes/loginUser");
@@ -79,6 +76,10 @@ var search = require("./routes/searchUser");
 app.use("/api/search", search);
 var currentuser = require("./routes/currentUser");
 app.use("/api/currentuser", currentuser);
+var followuser = require("./routes/following");
+app.use("/api/follow", followuser);
+var updatebio = require("./routes/updateBio");
+app.use("/api/updatebio", updatebio);
 
 //Listen on Port 8000
 const port = process.env.PORT || 8000;
@@ -88,37 +89,147 @@ var server = app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
 
-
 //Default path
 app.get("/", (req, res) => {
   res.send("Hello World Test!");
 });
 
 //path POST /upload
-app.post("/uploads", upload.single("myImage"), (req, res) => {
-  console.log(`File: ${req}`);
+app.post("/uploads", auth, upload.single("myImage"), async (req, res) => {
+  //Since the above middleware has already stored the image, the user document must
+  //now be updated with a reference to the image and the description for the photo.
+  const description = req.body.myDescription;
+  try {
+    //Find the user using the id and add a new post object containing the photoid and the description.
+    let userId = await User.findById(req.user.id);
+    await User.findByIdAndUpdate(
+      userId,
+      {
+        $addToSet: { posts: { photoid: req.file.id, description: description } }
+      },
+      { new: true, upsert: true },
+      (err, doc) => {
+        if (err) {
+          return res.status(400).json(err);
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+
+  //Return the stored file to the client.
   res.json({ file: req.file });
-
-
-//path POST /upload
-app.post("/uploads", upload.single("myImage"), (req, res) => {
-  console.log(`File: ${req}`);
-  res.json({ file: req.file });
 });
 
-app.post('/uploads', upload.single('myImage' ), (req, res) => {
-  console.log(`File: ${req}`)
-  res.json({file: req.file})
-  //res.json({file: req.file})
-  res.redirect('/');
+// @route GET /files
+// @desc Display all files in JSON
+app.get("/files", auth, async (req, res) => {
+  try {
+    //Search for all files stored by the requesting user.
+    const user = await User.findById(req.user.id);
+    let objectIdArray = user.posts.photoid.map(s => mongoose.Types.ObjectId(s));
+    gfs.files.find({ _id: { $in: objectIdArray } }).toArray((err, files) => {
+      //Check if file matching those stored in the users document exist. If not, return status 404.
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "No files exist"
+        });
+      }
 
+      //If file matching those stored in the users document exist, create "description" field and attach the description to the file objects.
+      for (var i = 0; i < files.length; i++) {
+        files[i]["description"] = user.posts.description[i];
+      }
+
+      //Return the files to the server.
+      return res.json(files);
+    });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
-//------------------MICHAEL MISO'S STUFF----------------------------------
-
-
+app.get("/files/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if file exists
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exist"
+      });
+    }
+    //If files exists, return file to client
+    return res.json(file);
+  });
 });
 
+//@route GET /image/:filename
+//@desc Display Image
+app.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    //Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exist"
+      });
+    }
+    //Check if image
+
+    if (
+      file.contentType === "image/jpeg" ||
+      file.contentType === "image/png" ||
+      file.contentType === "image/img" ||
+      file.contentType === "image/jpg" ||
+      file.contentType === "image/gif" ||
+      file.contentType === "image/tif"
+    ) {
+      //Read output to browser
+
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image"
+      });
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    //Check if files exist
+    if (!files || files.length === 0) {
+      res.render("profile", { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png" ||
+          file.contentType === "image/img" ||
+          file.contentType === "image/jpg" ||
+          file.contentType === "image/gif" ||
+          file.contentType === "image/tif"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+    }
+    res.sendFile({ files: files });
+    res.render("profile", { files: files });
+  });
+});
+
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+  });
+});
+
+//------------------MICHAEL MISO'S STUFF\----------------------------------
 //Schemas
 const Schema = mongoose.Schema;
 
@@ -133,120 +244,20 @@ const BlogPostSchema = new Schema({
 });
 
 //Models
-const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
-
+const BlogPost = mongoose.model("BlogPost", BlogPostSchema);
 
 // Route Test
-app.get('/api', (req, res) => {
-  BlogPost.find({  })
-      .then((data) => {
-          console.log('Data: ', data);
-          res.json(data);
-      })
-      .catch((error) => {
-          console.log('error: ', daerrorta);
-      });
+app.get("/api", (req, res) => {
+  BlogPost.find({})
+    .then(data => {
+      console.log("Data: ", data);
+      res.json(data);
+    })
+    .catch(error => {
+      console.log("error: ", daerrorta);
+    });
 });
 
 //------------------MICHAEL MISO'S STUFF^^^^^----------------------------------
-
-//@route GET /files/:filename
-// @desc Display all files in JSON
-app.get('/files', (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if(!files || files.length===0) {
-      return res.status(404).json({
-        err: 'No files exist'
-      });
-    }
-
-    //Files exist
-    return res.json(files);
-  })
-});
-
-
-app.get('/files/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    //Check if file
-    if (!file || file.length ===0){
-      return res.status(404).json({
-        err: 'No file exist'
-      });
-    }
-    // File exists
-    return res.json(file);
-  });
-});
-
-//@route GET /image/:filename
-//@desc Display Image
-
-app.get('/image/:filename', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    //Check if file
-    if (!file || file.length ===0){
-      return res.status(404).json({
-        err: 'No file exist'
-      });
-    }
-    //Check if image
-
-    if(file.contentType === 'image/jpeg' || 
-    file.contentType === 'image/png' || 
-    file.contentType === 'image/img' || 
-    file.contentType === 'image/jpg' || 
-    file.contentType === 'image/gif' || 
-    file.contentType === 'image/tif') {
-
-      //Read output to browser
-
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: 'Not an image'
-      });
-
-    }
-  });
-});
-
-app.get('/', (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    //Check if files exist
-    if(!files || files.length ===0){
-      res.render('profile', {files: false});
-    } else {
-      files.map(file => {
-        if(
-          file.contentType === 'image/jpeg' || 
-          file.contentType === 'image/png' || 
-          file.contentType === 'image/img' || 
-          file.contentType === 'image/jpg' || 
-          file.contentType === 'image/gif' || 
-          file.contentType === 'image/tif') 
-        {
-          file.isImage = true;
-        } else {
-          file.isImage = false;
-        }
-      });
-    }
-    res.sendFile({files:files});
-    res.render('profile', {files: files} );
-    });
-  });
-
-app.delete('/files/:id', (req, res) => {
-  gfs.remove({_id: req.params.id, root: 'upload'}, (err, gridStore) => {
-    if(err){
-      return res.status(404).json({ err: err});
-    }
-    res.redirect('/');
-  });
-});
-
 
 module.exports = server;
